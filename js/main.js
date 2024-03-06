10dd93ef849891e1aa785eb51f3ddc9f2eafe355
RoninsWork <47382042+yurkaronin@users.noreply.github.com>
2024-03-06 17:15:23 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+
+});
+
+// техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
+// получить в консоли элемент, по которому кликнули
+document.addEventListener('click', e => console.log(e.target));
